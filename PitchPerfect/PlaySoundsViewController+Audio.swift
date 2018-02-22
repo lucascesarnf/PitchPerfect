02de782e6ff7bea69e3149c021c389219330debb
lucascesarnf <lucascesarnf@hotmail.com>
2018-02-22 14:17:45 -0300

    first commit

---
+++ PitchPerfect/PlaySoundsViewController+Audio.swift
@@ -0,0 +1,9 @@
+//
+//  PlaySoundsViewController+Audio.swift
+//  PitchPerfect
+//
+//  Created by Lucas César  Nogueira Fonseca on 22/02/2018.
+//  Copyright © 2018 Lucas César  Nogueira Fonseca. All rights reserved.
+//
+
+import Foundation
---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Lucas César  Nogueira Fonseca on 20/02/2018.
+//  Copyright © 2018 Lucas César  Nogueira Fonseca. All rights reserved.
+//
+
+import UIKit
+
+class RecordSoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
